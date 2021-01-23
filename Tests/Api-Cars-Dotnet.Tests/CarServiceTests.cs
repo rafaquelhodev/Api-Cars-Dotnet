92a9bad36ad06390f39b04ca0c7592b6e30846b9
rafael.quelho <rafael.quelho@norus.com.br>
2021-01-23 17:47:25 -0300

    Test on create

---
+++ Tests/Api-Cars-Dotnet.Tests/CarServiceTests.cs
@@ -0,0 +1,41 @@
+using System;
+using Api_Cars_Dotnet.Models;
+using Api_Cars_Dotnet.Services;
+using AutoFixture;
+using MongoDB.Driver;
+using Moq;
+using Xunit;
+
+namespace Api_Cars_Dotnet.Tests
+{
+    public class CarServiceTests
+    {
+        private Fixture _fixture = new Fixture();
+        private readonly Mock<ICarStoreDatabaseSettings> _dbSettingMock = new Mock<ICarStoreDatabaseSettings>();
+
+        [Fact(DisplayName = "Create car in DB")]
+        public void InsertOneShouldBeCalledOnceOnCreation()
+        {
+            var collectionMock = new Mock<IMongoCollection<Car>>();
+            var dbMock = new Mock<IMongoDatabase>();
+
+            dbMock.Setup(x => x.GetCollection<Car>(It.IsAny<string>(), null)).Returns(collectionMock.Object);
+            collectionMock.Setup(x => x.InsertOne(It.IsAny<Car>(), null, default)).Verifiable();
+
+            _dbSettingMock.Setup(m => m.ConnectionString).Returns(_fixture.Create<string>);
+            _dbSettingMock.Setup(m => m.DatabaseName).Returns(_fixture.Create<string>);
+
+            // Act
+            var carService = new CarService(dbMock.Object, _dbSettingMock.Object);
+            Assert.NotNull(carService);
+
+            carService.Create(new Car
+            {
+                Age = 10
+            });
+
+            collectionMock.Verify(x => x.InsertOne(It.IsAny<Car>(), null, default), Times.Once());
+        }
+    }
+
+}
