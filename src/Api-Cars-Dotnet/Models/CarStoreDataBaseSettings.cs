e83448208c9ac04b7256a4003399eb15885f3186
rafael.quelho <rafael.quelho@norus.com.br>
2021-01-25 19:46:43 -0300

    Changing the way the connections are stored

--- src/Api-Cars-Dotnet/Models/CarStoreDataBaseSettings.cs
+++ src/Api-Cars-Dotnet/Models/CarStoreDataBaseSettings.cs
@@ -1,16 +1,9 @@
 namespace Api_Cars_Dotnet.Models
 {
-    public class CarStoreDatabaseSettings : ICarStoreDatabaseSettings
+    public class CarStoreDatabaseSettings : IDatabaseSettings
     {
-        public string CarsCollectionName { get; set; }
+        public string CollectionName { get; set; }
         public string ConnectionString { get; set; }
         public string DatabaseName { get; set; }
-    }
-
-    public interface ICarStoreDatabaseSettings
-    {
-        string CarsCollectionName { get; set; }
-        string ConnectionString { get; set; }
-        string DatabaseName { get; set; }
     }
 }
