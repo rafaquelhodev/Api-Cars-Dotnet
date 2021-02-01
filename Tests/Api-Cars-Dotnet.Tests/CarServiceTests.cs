5ea0ff17c7baf06aead344ae90e135c0adca8a9e
rafael.quelho <rafael.quelho@norus.com.br>
2021-02-01 20:23:02 -0300

    Update CarServiceTests.cs

--- Tests/Api-Cars-Dotnet.Tests/CarServiceTests.cs
+++ Tests/Api-Cars-Dotnet.Tests/CarServiceTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Api_Cars_Dotnet.Models;
 using Api_Cars_Dotnet.Services;
 using AutoFixture;
@@ -11,46 +12,56 @@ namespace Api_Cars_Dotnet.Tests
     public class CarServiceTests
     {
         private Fixture _fixture = new Fixture();
-        private readonly Mock<ICarStoreDatabaseSettings> _dbSettingMock = new Mock<ICarStoreDatabaseSettings>();
+        private readonly Mock<IDatabaseSettings> _dbSettingMock = new Mock<IDatabaseSettings>();
+        private readonly Mock<IRepository<Car>> repositoryMock = new Mock<IRepository<Car>>();
 
         [Fact(DisplayName = "Create car in DB")]
         public void InsertOneShouldBeCalledOnceOnCreation()
         {
-            var collectionMock = new Mock<IMongoCollection<Car>>();
-            var dbMock = new Mock<IMongoDatabase>();
-
-            dbMock.Setup(x => x.GetCollection<Car>(It.IsAny<string>(), null)).Returns(collectionMock.Object);
-            collectionMock.Setup(x => x.InsertOne(It.IsAny<Car>(), null, default)).Verifiable();
-
-            _dbSettingMock.Setup(m => m.ConnectionString).Returns(_fixture.Create<string>);
-            _dbSettingMock.Setup(m => m.DatabaseName).Returns(_fixture.Create<string>);
-
-            // Act
-            var carService = new CarService(dbMock.Object, _dbSettingMock.Object);
-            Assert.NotNull(carService);
-
-            carService.Create(new Car
+            var car = new Car
             {
                 Age = 10
-            });
+            };
 
-            collectionMock.Verify(x => x.InsertOne(It.IsAny<Car>(), null, default), Times.Once());
+            // var repositoryMock = new Mock<IRepository<Car>>();
+
+            repositoryMock.Reset();
+
+            repositoryMock.Setup(x => x.Insert(It.IsAny<Car>())).Returns(car);
+
+            // Act
+            var carService = new CarService(repositoryMock.Object);
+            Assert.NotNull(carService);
+
+            carService.Create(car);
+
+            repositoryMock.Verify(x => x.Insert(It.IsAny<Car>()), Times.Once());
         }
 
         [Fact(DisplayName = "Create invalid car")]
         public void InsertCarWithInvalidAgeOnCreation()
         {
-            var collectionMock = new Mock<IMongoCollection<Car>>();
-            var dbMock = new Mock<IMongoDatabase>();
+            // var collectionMock = new Mock<IMongoCollection<Car>>();
+            // var dbMock = new Mock<IMongoDatabase>();
 
-            dbMock.Setup(x => x.GetCollection<Car>(It.IsAny<string>(), null)).Returns(collectionMock.Object);
-            collectionMock.Setup(x => x.InsertOne(It.IsAny<Car>(), null, default)).Verifiable();
+            // dbMock.Setup(x => x.GetCollection<Car>(It.IsAny<string>(), null)).Returns(collectionMock.Object);
+            // collectionMock.Setup(x => x.InsertOne(It.IsAny<Car>(), null, default)).Verifiable();
 
-            _dbSettingMock.Setup(m => m.ConnectionString).Returns(_fixture.Create<string>);
-            _dbSettingMock.Setup(m => m.DatabaseName).Returns(_fixture.Create<string>);
+            // _dbSettingMock.Setup(m => m.ConnectionString).Returns(_fixture.Create<string>);
+            // _dbSettingMock.Setup(m => m.DatabaseName).Returns(_fixture.Create<string>);
+
+            var car = new Car
+            {
+                Age = -10
+            };
+
+            repositoryMock.Reset();
+
+            // var repositoryMock = new Mock<IRepository<Car>>();
 
             // Act
-            var carService = new CarService(dbMock.Object, _dbSettingMock.Object);
+            var carService = new CarService(repositoryMock.Object);
+
             Assert.NotNull(carService);
 
             var invalidCar = new Car
@@ -60,7 +71,33 @@ namespace Api_Cars_Dotnet.Tests
 
             Assert.Throws<ApplicationException>(() => carService.Create(invalidCar));
 
-            collectionMock.Verify(x => x.InsertOne(It.IsAny<Car>(), null, default), Times.Never());
+            repositoryMock.Verify(x => x.Insert(It.IsAny<Car>()), Times.Never());
+        }
+
+        [Fact(DisplayName = "Get car by Id")]
+        public void GetCarById()
+        {
+            var car = new Car
+            {
+                Id = "teste",
+                Age = 10,
+                Price = 1000.0m,
+                Brand = "tesla",
+                Color = "black"
+            };
+
+            repositoryMock.Reset();
+
+            repositoryMock.Setup(x => x.GetById(It.IsAny<string>())).Returns(car);
+
+            var carService = new CarService(repositoryMock.Object);
+            Assert.NotNull(carService);
+
+            var carFound = carService.Get(car.Id);
+
+            Assert.Equal(car, carFound);
+
+            repositoryMock.Verify(x => x.GetById(It.IsAny<string>()), Times.Once());
         }
     }
 
