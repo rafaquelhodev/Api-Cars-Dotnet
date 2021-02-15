61d2cbe10c4ed04e9274a61c7db80464a254d5a7
rafael.quelho <rafael.quelho@norus.com.br>
2021-02-15 11:08:14 -0300

    testing service

--- Tests/Api-Cars-Dotnet.Tests/CarServiceTests.cs
+++ Tests/Api-Cars-Dotnet.Tests/CarServiceTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using Api_Cars_Dotnet.Models;
 using Api_Cars_Dotnet.Services;
@@ -86,6 +87,116 @@ namespace Api_Cars_Dotnet.Tests
             Assert.Equal(car, carFound);
 
             repositoryMock.Verify(x => x.GetById(It.IsAny<string>()), Times.Once());
+        }
+
+        [Fact(DisplayName = "Get all")]
+        public void GetAllCars()
+        {
+            var cars = _fixture.CreateMany<Car>(5).ToList();
+
+            repositoryMock.Reset();
+
+            repositoryMock.Setup(x => x.GetAll()).Returns(cars);
+
+            // Act
+            var carService = new CarService(repositoryMock.Object);
+            Assert.NotNull(carService);
+
+            var result = carService.Get();
+
+            Assert.Equal(cars, result);
+        }
+
+        [Fact(DisplayName = "Get by Id")]
+        public void GetById()
+        {
+            var car = new Car
+            {
+                Id = "1",
+                Color = "black",
+                Brand = "tesla"
+            };
+
+            repositoryMock.Reset();
+
+            repositoryMock.Setup(x => x.GetById(car.Id)).Returns(car);
+
+            // Act
+            var carService = new CarService(repositoryMock.Object);
+            Assert.NotNull(carService);
+
+            var result = carService.Get(car.Id);
+
+            Assert.Equal(car, result);
+        }
+
+        [Fact(DisplayName = "Update")]
+        public void Update()
+        {
+            var car = new Car
+            {
+                Id = "1",
+                Color = "black",
+                Brand = "tesla"
+            };
+
+            repositoryMock.Reset();
+
+            repositoryMock.Setup(x => x.Update(It.IsAny<string>(), It.IsAny<Car>())).Verifiable();
+
+            // Act
+            var carService = new CarService(repositoryMock.Object);
+            Assert.NotNull(carService);
+
+            carService.Update(car.Id, car);
+
+            repositoryMock.Verify(x => x.Update(It.IsAny<string>(), It.IsAny<Car>()), Times.Once());
+        }
+
+        [Fact(DisplayName = "Remove with object as input")]
+        public void RemoveByObjectInput()
+        {
+            var car = new Car
+            {
+                Id = "1",
+                Color = "black",
+                Brand = "tesla"
+            };
+
+            repositoryMock.Reset();
+
+            repositoryMock.Setup(x => x.Delete(It.IsAny<Car>())).Verifiable();
+
+            // Act
+            var carService = new CarService(repositoryMock.Object);
+            Assert.NotNull(carService);
+
+            carService.Remove(car);
+
+            repositoryMock.Verify(x => x.Delete(It.IsAny<Car>()), Times.Once());
+        }
+
+        [Fact(DisplayName = "Remove with Id as input")]
+        public void RemoveById()
+        {
+            var car = new Car
+            {
+                Id = "1",
+                Color = "black",
+                Brand = "tesla"
+            };
+
+            repositoryMock.Reset();
+
+            repositoryMock.Setup(x => x.Delete(It.IsAny<string>())).Verifiable();
+
+            // Act
+            var carService = new CarService(repositoryMock.Object);
+            Assert.NotNull(carService);
+
+            carService.Remove(car.Id);
+
+            repositoryMock.Verify(x => x.Delete(It.IsAny<string>()), Times.Once());
         }
 
 
